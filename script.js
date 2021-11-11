c5c49a3d8d419345c7b05dcad242e623c77e5003
David Davies <David_Davies@mail.com>
2021-11-11 17:33:49 +0000

    added javascript.  started writing the quiz function, getting the quiz data, resetting buttons and getting selected answer.

--- script.js
+++ script.js
@@ -91,7 +91,8 @@ var codeQuizData = [
     correct: "#",
   },
 ];
-
+//-------------------------------------------------------------//
+// setting the required variables for the quiz                 //
 var quiz = document.getElementById("quiz");
 var timer = document.querySelector("#timer"); 
 var answerElements = document.querySelectorAll(".answer");
@@ -104,16 +105,45 @@ var submitBtn = document.getElementById("submit_Btn");
 
 var currentQuiz = 0;
 var score = 0;
-
+//----------------------------------------------------------------//
+//when the cards flips the writing also flips backwards these transforms turn them back
 document.getElementById("startQuiz").onclick = function changeContent() {
   document.getElementById("flipMain").style.transform = "rotatey(180deg)";
   document.getElementById("flipInner").style.transform = "rotatey(180deg)";
   document.getElementById("quizHeader").style.transform = "rotatey(180deg)";
   document.getElementById("results").style.transform = "rotatey(180deg)";
+  document.getElementById("highScoreTable").style.transform = "rotatey(180deg)";
 };
-
+//----------------------------------------------------------------//
+// initally the when function is called in get the quiz data -----//
 codeQuizLoad();
 
 function codeQuizLoad() {
+  deselectAnswers();
 
+  var currentQuizData = codeQuizData[currentQuiz];
+
+  questionElements.innerText = currentQuizData.question;
+  answerA.innerText = currentQuizData.a;
+  answerB.innerText = currentQuizData.b;
+  answerC.innerText = currentQuizData.c;
+  answerD.innerText = currentQuizData.d;
 }
+//-------------------------------------------------------------||
+//delselects all answers at the start of new quiz
+function deselectAnswers() {
+  answerElements.forEach((answerElements) => (answerElements.checked = false));
+}
+//-------------------------------------------------------------||
+//looks to see which answer has been selected and stores it
+function getSelected() {
+  var answer;
+
+  answerElements.forEach((answerElements) => {
+    if (answerElements.checked) {
+      answer = answerElements.id;
+    }
+  });
+  return answer;
+}
+//-------------------------------------------------------------||
